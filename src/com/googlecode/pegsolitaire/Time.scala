fc19e3c9156e1bbf58217ca5eae573e2d5859f4b
Bernd Amend <bernd.amend@googlemail.com>
2010-11-07 23:14:34 +0100

    prettify Time output

--- src/com/googlecode/pegsolitaire/Time.scala
+++ src/com/googlecode/pegsolitaire/Time.scala
@@ -18,15 +18,62 @@
 package com.googlecode.pegsolitaire
 
 object Time {
-	def apply[T](name: String)(block: => T) {
+	def apply[T](name: String, output: String => Unit = println)(block: => T) = {
 		val start = System.currentTimeMillis
+		var result: T = null.asInstanceOf[T]
 		try {
-			block
+			result = block
 		} finally {
-			val diff = System.currentTimeMillis - start
-			val seconds = diff / 1000.0
-			val minutes = seconds / 60.0
-			println(name + " completed, time taken: " + diff + " ms = " + seconds + " sec = " + minutes + " min")
+			val time			= System.currentTimeMillis - start
+
+			val milliseconds	= (time % 1000L, time / 1000L)
+			val seconds			= (milliseconds._2 % 60L, milliseconds._2 / 60L)
+			val minutes			= (seconds._2 % 60L, seconds._2 / 60L)
+			val hours			= (minutes._2 % 24L, minutes._2 / 24L)
+			val days			= (hours._2 % 24L, 0)
+
+			val str = new StringBuilder
+			str append name
+			str append " (took"
+
+			if(days._1 > 0) {
+				str append " "
+				str append days._1
+				str append (if(days._1 == 1) " day" else " days")
+			}
+
+			if(hours._1 > 0) {
+				str append " "
+				str append hours._1
+				str append (if(hours._1 == 1) " hour" else " hours")
+			}
+
+			if(minutes._1 > 0) {
+				str append " "
+				str append minutes._1
+				str append (if(minutes._1 == 1) " minute" else " minutes")
+			}
+
+			if(seconds._1 > 0) {
+				str append " "
+				str append seconds._1
+				str append (if(seconds._1 == 1) " second" else " seconds")
+			}
+
+			if(milliseconds._1 > 0) {
+				str append " "
+				str append milliseconds._1
+				str append (if(milliseconds._1 == 1) " millisecond" else " milliseconds")
+			}
+
+			if(time == 0) {
+				str append " 0 milliseconds"
+			}
+
+			str append ")"
+
+			output(str.toString)
 		}
+		result
 	}
 }
