ac2bf35ac9425878766aa7d98a0537d7bde1eec6
Bernd Amend <bernd.amend@googlemail.com>
2012-10-05 18:14:48 +0200

    move CodeGenerator to Board

--- peg-solitaire/src/com/googlecode/pegsolitaire/Board.scala
+++ peg-solitaire/src/com/googlecode/pegsolitaire/Board.scala
@@ -212,6 +212,58 @@ final class Board(val boardDescription: String, val moveDirections: Array[MoveDi
 		movemask_transformation_tests forall{i => movemask.contains(toLong(transformation(i)))}
 	}
 
+	private def generateCode(in: Array[Array[Int]]): String = {
+		val field: Array[Int] = (in.flatten.filter( _ != -1))
+
+		// calculate operations
+		val output = new scala.collection.mutable.HashMap[Int, Long]
+
+		for (i <- (field.length-1).to(0,-1)) {
+			val mask = 1L << i
+
+			val e = field(field.length-1-i)
+			val diff = e - i
+
+			if(output contains diff) {
+			  output(diff) |= mask
+			} else {
+			  output(diff) = mask
+			}
+		}
+
+		// generate code
+		val result = new StringBuilder
+
+		result append "(\n   "
+		var pos = 0
+		for(i <- output) {
+			result append "((f & "
+			result append i._2
+			result append "L)"
+
+			if (i._1 > 0) {
+				result append " << "
+				result append math.abs(i._1)
+			} else if (i._1 < 0) {
+				result append " >> "
+				result append math.abs(i._1)
+			}
+			result append ")"
+
+			if(pos%4 == 3)
+				result append "\n"
+
+			if(pos != output.size-1)
+				result append " | "
+
+			pos += 1
+		}
+
+		result append ")"
+
+		result.result()
+	}
+
 	private val _board_helper_sourcecode: String = {
 		val get_normalform = new StringBuilder
 		val get_equivalent_fields = new StringBuilder
@@ -224,7 +276,7 @@ final class Board(val boardDescription: String, val moveDirections: Array[MoveDi
 		get_equivalent_fields append "n += f\n\n"
 
 		if(is_transformation_valid(rotate180)) {
-			val c_rotate180 = CodeGenerator(rotate180(lookUpTable))
+			val c_rotate180 = generateCode(rotate180(lookUpTable))
 			get_normalform append "val n180 = " + c_rotate180 + "\n"
 			get_normalform append "if(n180 < n) n = n180\n\n"
 
@@ -232,7 +284,7 @@ final class Board(val boardDescription: String, val moveDirections: Array[MoveDi
 		}
 
 		if(is_transformation_valid(rotate90)) {
-			val c_rotate90 = CodeGenerator(rotate90(lookUpTable))
+			val c_rotate90 = generateCode(rotate90(lookUpTable))
 			get_normalform append "val n90 = " + c_rotate90 + "\n"
 			get_normalform append "if(n90 < n) n = n90\n\n"
 
@@ -240,7 +292,7 @@ final class Board(val boardDescription: String, val moveDirections: Array[MoveDi
 		}
 
 		if(is_transformation_valid(rotate270)) {
-			val c_rotate270 = CodeGenerator(rotate270(lookUpTable))
+			val c_rotate270 = generateCode(rotate270(lookUpTable))
 			get_normalform append "val n270 = " + c_rotate270 + "\n"
 			get_normalform append "if(n270 < n) n = n270\n\n"
 
@@ -248,7 +300,7 @@ final class Board(val boardDescription: String, val moveDirections: Array[MoveDi
 		}
 
 		if(is_transformation_valid(vflip)) {
-			val c_vflip = CodeGenerator(vflip(lookUpTable))
+			val c_vflip = generateCode(vflip(lookUpTable))
 			get_normalform append "val v = " + c_vflip + "\n"
 			get_normalform append "if(v < n) n = v\n\n"
 
@@ -256,7 +308,7 @@ final class Board(val boardDescription: String, val moveDirections: Array[MoveDi
 		}
 
 		if(is_transformation_valid(hflip)) {
-			val c_hflip = CodeGenerator(hflip(lookUpTable))
+			val c_hflip = generateCode(hflip(lookUpTable))
 			get_normalform append "val h = " + c_hflip + "\n"
 			get_normalform append "if(h < n) n = h\n\n"
 
@@ -264,7 +316,7 @@ final class Board(val boardDescription: String, val moveDirections: Array[MoveDi
 		}
 
 		if(is_transformation_valid(vflip_rotate90)) {
-			val c_v90 = CodeGenerator(vflip_rotate90(lookUpTable))
+			val c_v90 = generateCode(vflip_rotate90(lookUpTable))
 			get_normalform append "val v90 = " + c_v90 + "\n"
 			get_normalform append "if(v90 < n) n = v90\n\n"
 
@@ -272,7 +324,7 @@ final class Board(val boardDescription: String, val moveDirections: Array[MoveDi
 		}
 
 		if(is_transformation_valid(hflip_rotate90)) {
-			val c_h90 = CodeGenerator(hflip_rotate90(lookUpTable))
+			val c_h90 = generateCode(hflip_rotate90(lookUpTable))
 			get_normalform append "val h90 = " + c_h90 + "\n"
 			get_normalform append "if(h90 < n) n = h90\n\n"
 
